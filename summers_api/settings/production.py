44df835965aab92da95574d43598a15e0f84abcc
summerthe <summerthe43@gmail.com>
2022-12-14 19:06:36 +0530

    Add settings related to http->https redirect

--- summers_api/settings/production.py
+++ summers_api/settings/production.py
@@ -82,4 +82,13 @@ else:
     WHITENOISE_MANIFEST_STRICT = False
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+# settings related to http->https redirect
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Security Headers
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = 3600
